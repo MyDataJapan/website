7fd8e4abd17e1c51f3caeeace4560c526f8a68bb
Dixon Siu <siu.dixon@jp.fujitsu.com>
2018-12-05 22:02:05 +0900

    CHORES: Render recent events with JSON

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,11 @@ var fs = require("fs");
 var rename = require('gulp-rename');
 
 gulp.task('ejs', () => {
+    var recentEvents = JSON.parse(fs.readFileSync('_page/data/recent_events.json', 'utf8'));
     var pastEvents = JSON.parse(fs.readFileSync('_page/data/past_events.json', 'utf8'));
     gulp.src(['./_page/**/*.ejs'])
         .pipe(ejs({
+            recentEvents: recentEvents,
             pastEvents: pastEvents
         }))
         .pipe(rename({extname: '.html'}))
